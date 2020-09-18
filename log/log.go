5d56751ad43835b35c8d79df41726d40d79bfaa5
wangshuo <wangshuo@sky-cloud.net>
2020-09-18 15:38:18 +0800

    fix: add log

---
+++ log/log.go
@@ -0,0 +1,257 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+// NewLog init new console log
+func NewLog(l string, filePath string) *Log {
+	var (
+		err   error
+		level logLevel
+	)
+	if level, err = parseLogLevel(l); err != nil {
+		panic(err)
+	}
+	f1 := &Log{
+		level:       level,
+		filePath:    filePath,
+		maxFileSize: int64(1024 * 1024 * 256),
+		saveDays:    7,
+		consoleFlag: true,
+		textFlag:    true,
+		logNum:      1,
+		logChan:     make(chan *logMsg, 50000),
+	}
+	if err = f1.initFile(filePath); err != nil {
+		panic(err)
+	}
+	return f1
+}
+
+// SetSaveDays 设置日志文件数
+func (l *Log) SetSaveDays(days int) {
+	l.saveDays = days
+}
+
+func (l *Log) initFile(filePath string) error {
+	fileObj, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("open file error")
+	}
+	l.fileObj = fileObj
+	// 开启后台goroutine写日志
+	go l.writeLogBackground()
+	return nil
+}
+
+// Close close file
+func (l *Log) Close() {
+	l.fileObj.Close()
+}
+
+// SetLevel set log level
+func (l *Log) SetLevel(lv string) {
+	var (
+		err   error
+		level logLevel
+	)
+	if level, err = parseLogLevel(lv); err != nil {
+		fmt.Println(err)
+	}
+	l.level = level
+}
+
+// SetConsoleFlag set console print
+func (l *Log) SetConsoleFlag(flag bool) {
+	l.consoleFlag = flag
+}
+
+// SetTextFlag set text print
+func (l Log) SetTextFlag(flag bool) {
+	l.textFlag = flag
+}
+
+func (l *Log) enable(level logLevel) bool {
+	return l.level <= level
+}
+
+func (l *Log) console(lv logLevel, str string, msg ...interface{}) {
+	if l.enable(lv) {
+		now := time.Now()
+		funcName, fileName, lineNo := getInfo(3)
+		level, err := unparseLogLevel(lv)
+		if err != nil {
+			fmt.Printf("unparse LogLevel failed\n")
+		}
+		fmt.Printf("[%s] [%v] [%s:%s:%d] %s\n", now.Format("2006/01/02 15:04:05"), level, funcName, fileName, lineNo, fmt.Sprintf(str, msg...))
+	}
+}
+
+func (l *Log) writeLogBackground() {
+	if l.checkSize(l.fileObj) {
+		// 文件过大，分文件记录
+		l.fileObj.Close()
+		nowStr := time.Now().Format("20060102")
+		newLogName := fmt.Sprintf("%s.%s.%d.log", strings.TrimSuffix(l.filePath, ".log"), nowStr, l.logNum)
+		os.Rename(l.filePath, newLogName)
+		l.logNum++
+		fileObj, err := os.OpenFile(l.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("open file error\n")
+		}
+		l.fileObj = fileObj
+	}
+	if l.checkDays(l.fileObj) {
+		// 新一天，换新日志
+		l.fileObj.Close()
+		l.logNum = 1
+		nowStr := time.Now().Format("20060102")
+		newLogName := fmt.Sprintf("%s.%s.%d.log", strings.TrimSuffix(l.filePath, ".log"), nowStr, l.logNum)
+		os.Rename(l.filePath, newLogName)
+		fileObj, err := os.OpenFile(l.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("open file error\n")
+		}
+		l.fileObj = fileObj
+
+	}
+	for {
+		select {
+		case logTmp := <-l.logChan:
+			fmt.Fprintf(l.fileObj, "[%s] [%v] [%s:%s:%d] %s\n", logTmp.timestamp, logTmp.level, logTmp.funcName, logTmp.fileName, logTmp.lineNo, logTmp.msg)
+		default:
+			// 取不到日志，休息500ms
+			time.Sleep(time.Millisecond * 500)
+		}
+	}
+}
+
+func (l *Log) text(lv logLevel, str string, msg ...interface{}) {
+	if l.enable(lv) {
+		now := time.Now()
+		funcName, fileName, lineNo := getInfo(3)
+		level, err := unparseLogLevel(lv)
+		if err != nil {
+			fmt.Printf("unparse LogLevel failed\n")
+		}
+		// 先把日志发送到通道中
+		logTmp := &logMsg{
+			level:     level,
+			msg:       fmt.Sprintf(str, msg...),
+			funcName:  funcName,
+			fileName:  fileName,
+			timestamp: now.Format("2006/01/02 15:04:05"),
+			lineNo:    lineNo,
+		}
+		select {
+		case l.logChan <- logTmp:
+		default: // 丢掉日志，不出现阻塞
+		}
+
+	}
+}
+
+func (l *Log) checkSize(file *os.File) bool {
+	var (
+		err      error
+		fileinfo os.FileInfo
+	)
+	if fileinfo, err = file.Stat(); err != nil {
+		fmt.Printf("get file info failed, err:%v\n ", err)
+	}
+	// 如果当前文件大小大于等于日志文件的最大值，返回true
+	if fileinfo.Size() > l.maxFileSize {
+		return true
+	}
+	return false
+}
+
+func (l *Log) checkDays(file *os.File) bool {
+	// 如果当前文件日期大于等于日志文件的最大值，返回true
+	var (
+		err      error
+		fileinfo os.FileInfo
+	)
+	if fileinfo, err = file.Stat(); err != nil {
+		fmt.Printf("get file info failed, err:%v\n ", err)
+	}
+
+	if fileinfo.ModTime().Day() < time.Now().Day() {
+		l.delete()
+		return true
+	}
+	return false
+}
+
+func (l *Log) delete() {
+	filePath, _ := filepath.Abs(filepath.Dir(l.fileObj.Name()))
+	fakeNow := time.Now().AddDate(0, 0, -l.saveDays)
+	filepath.Walk(filePath,
+		func(path string, f os.FileInfo, err error) error {
+			if f == nil {
+				return err
+			}
+			// 防止误删
+			if !f.IsDir() && f.ModTime().Before(fakeNow) && strings.HasSuffix(f.Name(), l.fileObj.Name()) {
+				os.Remove(path)
+				return nil
+			}
+
+			return nil
+		})
+}
+
+// Debug Debug level
+func (l *Log) Debug(str string, msg ...interface{}) {
+	if l.consoleFlag {
+		l.console(DEBUG, str, msg...)
+	}
+	if l.textFlag {
+		l.text(DEBUG, str, msg...)
+	}
+}
+
+// Info Info level
+func (l *Log) Info(str string, msg ...interface{}) {
+	if l.consoleFlag {
+		l.console(INFO, str, msg...)
+	}
+	if l.textFlag {
+		l.text(INFO, str, msg...)
+	}
+}
+
+// Warning Warning level
+func (l *Log) Warning(str string, msg ...interface{}) {
+	if l.consoleFlag {
+		l.console(WARNING, str, msg...)
+	}
+	if l.textFlag {
+		l.text(WARNING, str, msg...)
+	}
+}
+
+// Error Error level
+func (l *Log) Error(str string, msg ...interface{}) {
+	if l.consoleFlag {
+		l.console(ERROR, str, msg...)
+	}
+	if l.textFlag {
+		l.text(ERROR, str, msg...)
+	}
+}
+
+// Fatal Fatal level
+func (l *Log) Fatal(str string, msg ...interface{}) {
+	if l.consoleFlag {
+		l.console(FATAL, str, msg...)
+	}
+	if l.textFlag {
+		l.text(FATAL, str, msg...)
+	}
+}
---
+++ log/utils.go
@@ -0,0 +1,95 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+)
+
+type logLevel uint16
+
+const (
+	// UNKNOWN UNKNOWN level
+	UNKNOWN logLevel = iota
+	// DEBUG DEBUG level
+	DEBUG
+	// INFO INFO level
+	INFO
+	// WARNING WARNING level
+	WARNING
+	// ERROR ERROR level
+	ERROR
+	// FATAL FATAL level
+	FATAL
+)
+
+type logMsg struct {
+	level     string
+	msg       string
+	funcName  string
+	fileName  string
+	timestamp string
+	lineNo    int
+}
+
+// Log log struct
+type Log struct {
+	level       logLevel     // 日志级别
+	filePath    string       // 文件路径
+	fileDays    string       // 记录文件日期
+	fileObj     *os.File     // 文件对象
+	maxFileSize int64        // 最大文件大小, 默认1024000
+	saveDays    int          // 文件保存天数
+	consoleFlag bool         // 是否在命令行打印日志，默认打印
+	textFlag    bool         // 是否记录到文件，默认记录
+	logNum      int64        // 当日文件数，默认为1
+	logChan     chan *logMsg // 日志文件缓冲区，支持异步日志写入
+}
+
+func parseLogLevel(level string) (logLevel, error) {
+	s := strings.ToUpper(level)
+	switch s {
+	case "DEBUG":
+		return DEBUG, nil
+	case "INFO":
+		return INFO, nil
+	case "WARNING":
+		return WARNING, nil
+	case "ERROR":
+		return ERROR, nil
+	case "FATAL":
+		return FATAL, nil
+	default:
+		return UNKNOWN, fmt.Errorf("unknown level")
+	}
+}
+
+func unparseLogLevel(lv logLevel) (string, error) {
+	switch lv {
+	case DEBUG:
+		return "DEBUG", nil
+	case INFO:
+		return "INFO", nil
+	case WARNING:
+		return "WARNING", nil
+	case ERROR:
+		return "ERROR", nil
+	case FATAL:
+		return "FATAL", nil
+	default:
+		return "UNKNOWN", fmt.Errorf("unknown level")
+	}
+}
+
+func getInfo(n int) (string, string, int) {
+	pc, file, line, ok := runtime.Caller(n)
+	if !ok {
+		fmt.Printf("runtime.Caller() failed\n")
+		return "", "", 0
+	}
+	funcName := runtime.FuncForPC(pc).Name()
+	fileName := path.Base(file)
+	return funcName, fileName, line
+}
